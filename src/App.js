ab84b19378a362606405e11bf7388ae430a9ec12
Gabriel Dias <g.dias1@hotmail.com>
2021-12-06 16:33:47 -0300

    Added title page without image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import TitleContainer from "./components/TitleContainer";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <>
+    <Wrapper>
       <Header />
-    </>
+      <TitleContainer />
+    </Wrapper>
   );
 }
 
---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+function Title() {
+  return (
+    <div>
+      <h1 className="mb-4">
+        Get in your own <br /> space.
+      </h1>
+      <h3>
+        The Rest Space was created to help you.
+        <br /> Sometimes you just need to take a breath
+      </h3>
+    </div>
+  );
+}
+
+export default Title;
